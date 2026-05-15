e170f061f920096cc452fe039459c17afb4a8050
Mohamad Rizal Nurochman <muhamadrizalnurohman072005@gmail.com>
2026-05-15 22:34:53 +0700

    add

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 import AuthBanner from '@/components/auth/AuthBanner';
 
-
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-white font-sans">
